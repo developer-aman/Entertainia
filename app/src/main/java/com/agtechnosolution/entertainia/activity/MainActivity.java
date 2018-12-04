97947173b418de78ae2d63d411867366084d9bb4
developer-aman <gupta.ag1993@gmail.com>
2018-12-04 15:23:28 +0530

    First Commit

--- app/src/main/java/com/agtechnosolution/entertainia/activity/MainActivity.java
+++ app/src/main/java/com/agtechnosolution/entertainia/activity/MainActivity.java
@@ -2,14 +2,18 @@ package com.agtechnosolution.entertainia.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
 import com.agtechnosolution.entertainia.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    RecyclerView moviesRecycler;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        moviesRecycler=findViewById(R.id.movies_recycler);
     }
 }
